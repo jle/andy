b077dc963b991b503ca123284c131e18db08b81a
Jonathan Le <jonathanle@gmail.com>
2013-04-02 13:37:28 -0700

    Fix LogView.

--- example/src/com/vandalsoftware/android/spdyexample/LogView.java
+++ example/src/com/vandalsoftware/android/spdyexample/LogView.java
@@ -5,18 +5,21 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Looper;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class LogView extends View {
     private int mY;
-    private String mMessage;
+    private ArrayList<String> mPendingMessages = new ArrayList<String>(20);
     private final TextPaint mPaint;
     private Bitmap mBitmap;
     private Canvas mBmCanvas;
-    private boolean mClear;
     private int mBottom;
+    private int mFontHeight;
 
     public LogView(Context context) {
         this(context, null);
@@ -29,7 +32,6 @@ public class LogView extends View {
     public LogView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
         mPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
-        mClear = true;
     }
 
     @Override
@@ -40,39 +42,40 @@ public class LogView extends View {
                 Bitmap.Config.ARGB_8888);
         mBmCanvas = new Canvas(mBitmap);
         mBottom = h;
+
+        final Paint.FontMetricsInt metrics = mPaint.getFontMetricsInt();
+        mFontHeight = metrics.descent - metrics.ascent;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Canvas bc = mBmCanvas;
-        if (mClear) {
+        boolean clear = mY + (mPendingMessages.size() * mFontHeight) >= mBottom || mY == 0;
+        if (clear) {
+            mY = mFontHeight;
             Paint.Style oldStyle = mPaint.getStyle();
             mPaint.setStyle(Paint.Style.FILL);
-            mPaint.setColor(Color.WHITE);
+            mPaint.setColor(Color.BLACK);
             bc.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint);
             mPaint.setStyle(oldStyle);
-            mClear = false;
         }
-        if (mMessage != null) {
-            mPaint.setColor(Color.BLACK);
-            bc.drawText(mMessage, getPaddingLeft(), getPaddingTop() + mY, mPaint);
-            mMessage = null;
+        if (!mPendingMessages.isEmpty()) {
+            mPaint.setColor(Color.WHITE);
+            for (String message : mPendingMessages) {
+                bc.drawText(message, getPaddingLeft(), getPaddingTop() + mY, mPaint);
+                mY += mFontHeight;
+            }
+            mPendingMessages.clear();
         }
         canvas.drawBitmap(mBitmap, 0, 0, mPaint);
     }
 
     public void log(String message) {
-        final Paint.FontMetricsInt metrics = mPaint.getFontMetricsInt();
-        int fh = metrics.descent - metrics.ascent;
-        if (mY >= mBottom) {
-            mY = fh;
-            mClear = true;
-        } else {
-            mY += fh;
-            mClear = false;
+        if (Thread.currentThread() != Looper.getMainLooper().getThread()) {
+            throw new IllegalThreadStateException("Calling from wrong thread.");
         }
-        mMessage = message;
+        mPendingMessages.add(message);
         invalidate();
     }
 }
